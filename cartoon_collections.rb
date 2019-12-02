7227ee77f3e61597371c7bad910a4acbc44d9dcb
Olena Matokhina <olena@matokhina.com>
2019-12-02 20:56:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.reduce
-  {|memo, call| 
+  planeteer_calls.reduce |memo, call| 
   do 
     puts "!!!!!!!"
     puts memo 
