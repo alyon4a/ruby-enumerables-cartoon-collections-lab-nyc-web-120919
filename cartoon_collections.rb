d0e87439cb6db560671943360446a17cfd326330
Olena Matokhina <olena@matokhina.com>
2019-12-03 02:13:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,5 @@ def find_the_cheese(look_for_cheese_here)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   look_for_cheese_here.detect() { |is_this_cheese|
-  cheese_types.include?(is_this_cheese) ? is_this_cheese : nil }
+  cheese_types.include?(is_this_cheese) }
 end
