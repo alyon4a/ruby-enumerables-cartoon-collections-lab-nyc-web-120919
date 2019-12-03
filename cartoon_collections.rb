0d2c8785f14a860c026519957351c9b73836e381
Olena Matokhina <olena@matokhina.com>
2019-12-03 01:44:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,13 +7,13 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.reduce |memo, call| 
+  planeteer_calls.reduce { |memo, call| 
   do 
     puts "!!!!!!!"
     puts memo 
     puts call
     call.length > 4 ? memo = true : (memo || false) 
-  end
+  end }
 end
 
 def find_the_cheese# code an argument here
